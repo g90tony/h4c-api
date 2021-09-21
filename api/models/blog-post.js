4dd1986acd9a81aeef01ed043b7936394eee319b
Caleb Tony <calebmbugua@gmail.com>
2021-09-21 21:20:33 +0300

    completed blog_post schema

---
+++ api/models/blog-post.js
@@ -0,0 +1,54 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const { sequelize } = require("../config/db");
+
+const Blog = sequelize.define("Blog_Post", {
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  image: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  views: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    defaultValue: 0,
+  },
+  publishedOn: {
+    type: DataTypes.DATE,
+    allowNull: true,
+    defaultValue: Sequelize.NOW,
+  },
+  author: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  content: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  tags: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  status: {
+    type: DataTypes.STRING,
+    allowNull: true,
+    defaultValue: "Draft",
+  },
+});
+
+async function syncBlogTable() {
+  try {
+    await Blog.sync({ force: false, alter: true });
+    console.info("All tables were created successfully");
+  } catch (error) {
+    console.error("There was a problem creating tables", error);
+  }
+}
+
+module.exports = {
+  Blog,
+  syncBlogTable,
+};
