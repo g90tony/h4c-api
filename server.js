d4d8888ba1ec1aa3b429c99083c4a6692de7c8c3
Caleb Tony <calebmbugua@gmail.com>
2021-09-21 21:08:51 +0300

    added sync blog post table

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const { sequelize, validate } = require("./api/config/db");
+const { syncBlogTable } = require("./api/models/blog-post");
 
 const app = express();
+
+// confirm that the database connected successfully
+validate();
+
+// create Blog Table if not created
+syncBlogTable();
 
 const PORT = process.env.PORT || 3000;
 
