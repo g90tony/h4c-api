dde0776c7e74d3ae48e6fcfe46435eb42d1c2198
Caleb Tony <calebmbugua@gmail.com>
2021-09-23 12:08:24 +0300

    completed gallery-image controller

---
+++ api/controllers/gallery.js
@@ -0,0 +1,51 @@
+const { GalleryImage } = require("../models/gallery-image");
+
+async function saveImage(payload) {
+  try {
+    new_image = await GalleryImage.create({
+      public_id: payload.public_id,
+      version: payload.version,
+      signature: payload.signature,
+      format: payload.format,
+      resource_type: payload.resource_type,
+      url: payload.url,
+      secure_url: payload.secure_url,
+    });
+
+    return {
+      status: "success",
+      data: new_image,
+    };
+  } catch (error) {
+    return {
+      status: "failed",
+      data: error,
+    };
+  }
+}
+
+async function fetchSingleImage(image_id) {
+  try {
+    fetched_image = await GalleryImage.findByPk(image_id);
+
+    return { status: "success", data: fetched_image };
+  } catch (error) {
+    return { status: "failed", data: error };
+  }
+}
+
+async function fetchImages(current_page = 1) {
+  try {
+    fetched_images = await GalleryImage.findAll();
+
+    return { status: "success", data: fetched_images };
+  } catch (error) {
+    return { status: "failed", data: error };
+  }
+}
+
+module.exports = {
+  fetchSingleImage,
+  fetchImages,
+  saveImage,
+};
