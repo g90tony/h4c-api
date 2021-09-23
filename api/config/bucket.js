2755360815e2010d56e4897c77cdd8808e1f0f81
Caleb Tony <calebmbugua@gmail.com>
2021-09-23 18:04:03 +0300

    added cloudinary bucket configurations

---
+++ api/config/bucket.js
@@ -0,0 +1,21 @@
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: process.env.cloud_name,
+  api_key: process.env.api_key,
+  api_secret: process.env.api_secret,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "DEV",
+  },
+});
+
+const upload = multer({ storage: storage });
+
+module.exports = {
+  cloudinary,
+  upload,
+};
