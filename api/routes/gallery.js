8f1f5d8608a64ee4aedfafddc102e8018f6f090e
Caleb Tony <calebmbugua@gmail.com>
2021-09-27 10:21:27 +0300

    updated response objects

--- api/routes/gallery.js
+++ api/routes/gallery.js
@@ -8,9 +8,19 @@ router.get("/get/image/:id", (req, res) => {
   controller_response = GalleryImageController.fetchSingleImage(req.params.id);
 
   if (controller_response.status === "success") {
-    res.status(200).json(controller_response.data);
+    res
+      .status(200)
+      .json({
+        message: "Successfully fetched image.",
+        data: controller_response.data,
+      });
   } else {
-    res.status(500).json(controller_response.data);
+    res
+      .status(500)
+      .json({
+        message: "There was a problem fetching the image",
+        error: controller_response.data,
+      });
   }
 });
 
@@ -20,18 +30,59 @@ router.get("/get/image/:page_number", (req, res) => {
   );
 
   if (controller_response.status === "success") {
-    res.status(200).json(controller_response.data);
+    res
+      .status(200)
+      .json({
+        message: "Successfully fetched page images ",
+        data: controller_response.data,
+      });
   } else {
-    res.status(500).json(controller_response.data);
+    res
+      .status(500)
+      .json({
+        message: "There was a problem fetching page images",
+        error: controller_response.data,
+      });
   }
 });
 
-router.post("/upload/image", upload.single(image), (req, res) => {
-  upload_image = req.file;
+router.post("/upload/image/many", upload.arrays(images), (req, res) => {
+  upload_images = req.files;
 
-  controller_responser = GalleryImageController.saveImage(response);
+  controller_responser = GalleryImageController.saveImages(upload_images);
 
   if ((controller_response.status = "success")) {
-    res.status(200).json(controller_response.data);
+    res
+      .status(200)
+      .json({
+        message: "Successfully uploaded image",
+        data: controller_response.data,
+      });
+  } else {
+    res.status(500).json({
+      message: "There was a problem uploading the image",
+      error: controller_response.data,
+    });
   }
 });
+router.post("/upload/image/one", upload.single(image), (req, res) => {
+  upload_image = req.file;
+
+  controller_responser = GalleryImageController.saveImage(upload_image);
+
+  if ((controller_response.status = "success")) {
+    res
+      .status(200)
+      .json({
+        message: "Successfully uploaded all the images",
+        data: controller_response.data,
+      });
+  } else {
+    res.status(500).json({
+      message: "There was a problem uploading the images",
+      error: controller_response.data,
+    });
+  }
+});
+
+module.exports = router;
