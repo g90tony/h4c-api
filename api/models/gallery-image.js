97011714f414859265ffcbb66250230264b0731f
Caleb Tony <calebmbugua@gmail.com>
2021-09-27 13:05:46 +0300

    reworke entire schema

--- api/models/gallery-image.js
+++ api/models/gallery-image.js
@@ -2,31 +2,31 @@ const { Sequelize, DataTypes } = require("sequelize");
 const { sequelize } = require("../config/db");
 
 const GalleryImage = sequelize.define("Gallery_Image", {
-  public_id: {
+  fieldname: {
     type: DataTypes.STRING,
     allowNull: false,
   },
-  version: {
+  originalname: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  encoding: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  mimetype: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  path: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  size: {
     type: DataTypes.INTEGER,
     allowNull: false,
   },
-  signature: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  format: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  resource_type: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  url: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  secure_url: {
+  filename: {
     type: DataTypes.STRING,
     allowNull: false,
   },
