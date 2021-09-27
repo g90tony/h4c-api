fe49ead9e2ff0401729bc2b12308a712747de049
Caleb Tony <calebmbugua@gmail.com>
2021-09-27 10:30:22 +0300

    updated response objects

--- api/routes/blog.js
+++ api/routes/blog.js
@@ -9,9 +9,15 @@ router.get("/get/post/:id", (req, res) => {
   const blog_controller_response = BlogController.fetchSinglePost(post_id);
 
   if (blog_controller_response.status == "success") {
-    res.status(200).json(blog_controller_response.data);
+    res.status(200).json({
+      message: "Successfully fetched post.",
+      data: blog_controller_response.data,
+    });
   } else {
-    res.status(500).json(blog_controller_response.data);
+    res.status(500).json({
+      message: "There was a problem fetching the post.",
+      error: blog_controller_response.data,
+    });
   }
 });
 
@@ -21,9 +27,15 @@ router.get("/get/posts/all/:page_number", (req, res) => {
   const blog_controller_response = BlogController.fetchPosts(current_page);
 
   if (blog_controller_response.status == "success") {
-    res.status(200).json(blog_controller_response.data);
+    res.status(200).json({
+      message: "Successfully fetched page posts.",
+      data: blog_controller_response.data,
+    });
   } else {
-    res.status(500).json(blog_controller_response.data);
+    res.status(500).json({
+      message: "There was a problem fetching the pages post",
+      error: blog_controller_response.data,
+    });
   }
 });
 router.get("/get/posts/published/:page_number", (req, res) => {
@@ -33,9 +45,15 @@ router.get("/get/posts/published/:page_number", (req, res) => {
     BlogController.fetchPublishedPosts(current_page);
 
   if (blog_controller_response.status == "success") {
-    res.status(200).json(blog_controller_response.data);
+    res.status(200).json({
+      message: "Successfully fetched pages published posts.",
+      data: blog_controller_response.data,
+    });
   } else {
-    res.status(500).json(blog_controller_response.data);
+    res.status(500).json({
+      message: "There was a problem fetching the pages published posts.",
+      error: blog_controller_response.data,
+    });
   }
 });
 router.get("/get/posts/draft/:page_number", (req, res) => {
@@ -44,19 +62,31 @@ router.get("/get/posts/draft/:page_number", (req, res) => {
   const blog_controller_response = BlogController.fetchDraftPosts(current_page);
 
   if (blog_controller_response.status == "success") {
-    res.status(200).json(blog_controller_response.data);
+    res.status(200).json({
+      message: "Successfully fetched pages draft posts",
+      data: blog_controller_response.data,
+    });
   } else {
-    res.status(500).json(blog_controller_response.data);
+    res.status(500).json({
+      message: "There was a problem fetching pages draft post",
+      error: blog_controller_response.data,
+    });
   }
 });
 
 router.post("/create/post", (req, res) => {
   const blog_controller_response = BlogController.create_post(req.body);
 
-  if (blog_controller_response.satus == "success") {
-    res.status(200).json(blog_controller_response.data);
+  if (blog_controller_response.status == "success") {
+    res.status(200).json({
+      message: "Successfully created post",
+      data: blog_controller_response.data,
+    });
   } else {
-    res.status(500).json(blog_controller_response.data);
+    res.status(500).json({
+      message: "There was a problem creating the blog post",
+      error: blog_controller_response.data,
+    });
   }
 });
 
@@ -66,9 +96,33 @@ router.put("update/post/publish/:id", (req, res) => {
   const blog_controller_response = BlogController.publishPost(post_id);
 
   if (blog_controller_response.status == "success") {
-    res.status(200).json(blog_controller_response.data);
+    res.status(200).json({
+      message: "Successfully published post",
+      data: blog_controller_response.data,
+    });
   } else {
-    res.status(500).json(blog_controller_response.data);
+    res.status(500).json({
+      message: "There was a problem publishing the post",
+      error: blog_controller_response.data,
+    });
+  }
+});
+
+router.put("update/post/views/:id", (req, res) => {
+  const post_id = req.params.id;
+
+  const blog_controller_response = BlogController.addView(post_id);
+
+  if (blog_controller_response.status == "success") {
+    res.status(200).json({
+      message: "Successfully updated post views",
+      data: blog_controller_response.data,
+    });
+  } else {
+    res.status(500).json({
+      message: "There was a problem updating the post views",
+      error: blog_controller_response.data,
+    });
   }
 });
 
@@ -78,9 +132,15 @@ router.delete("delete/post/:id", (req, res) => {
   const blog_controller_response = BlogController.deletePost(post_id);
 
   if (blog_controller_response.status === "success") {
-    res.status(200).json(blog_controller_response.data);
+    res.status(200).json({
+      message: "Successfully deleted the post",
+      data: blog_controller_response.data,
+    });
   } else {
-    res.status(500).json(blog_controller_response.data);
+    res.status(500).json({
+      message: "There was a problem deleting the post",
+      error: blog_controller_response.data,
+    });
   }
 });
 
