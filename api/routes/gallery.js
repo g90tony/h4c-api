ee8d77aa9d57c71db814f290c53d0fb0c5bcd300
Caleb Tony <calebmbugua@gmail.com>
2021-09-27 13:22:08 +0300

    minor edit

--- api/routes/gallery.js
+++ api/routes/gallery.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.get("/get/image/one/:id", GalleryImageController.fetchSingleImage);
 
-router.get("/get/images/all/:page_number", GalleryImageController.fetchImages);
+router.get("/get/image/all/:page_number", GalleryImageController.fetchImages);
 
 router.post(
   "/upload/image/many",
