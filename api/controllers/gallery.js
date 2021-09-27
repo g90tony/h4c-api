6739cef983ad501d8ee8d4f49591a9bf7032c003
Caleb Tony <calebmbugua@gmail.com>
2021-09-27 13:05:23 +0300

    refactored controllers

--- api/controllers/gallery.js
+++ api/controllers/gallery.js
@@ -1,76 +1,69 @@
 const { GalleryImage } = require("../models/gallery-image");
 
-async function saveImage(payload) {
+async function saveImage(req, res) {
   try {
     new_image = await GalleryImage.create({
-      public_id: payload.public_id,
-      version: payload.version,
-      signature: payload.signature,
-      format: payload.format,
-      resource_type: payload.resource_type,
-      url: payload.url,
-      secure_url: payload.secure_url,
+      fieldname: req.file.fieldname,
+      originalname: req.file.originalname,
+      encoding: req.file.encoding,
+      mimetype: req.file.mimetype,
+      path: req.file.path,
+      size: req.file.size,
+      filename: req.file.filename,
     });
 
-    return {
+    res.status(200).json({
       status: "success",
       data: new_image,
-    };
-  } catch (error) {
-    return {
-      status: "failed",
-      data: error,
-    };
-  }
-}
-
-async function saveImages(payload) {
-  try {
-    payload.map((image) => {
-      new_images = await GalleryImage.create({
-        public_id: payload.public_id,
-        version: payload.version,
-        signature: payload.signature,
-        format: payload.format,
-        resource_type: payload.resource_type,
-        url: payload.url,
-        secure_url: payload.secure_url,
-      });
-
-      return new_image;
     });
-
-    return {
-      status: "success",
-      data: new_images,
-    };
   } catch (error) {
-    return {
+    res.status(500).json({
       status: "failed",
       data: error,
-    };
+    });
   }
 }
 
-async function fetchSingleImage(image_id) {
+async function saveImages(req, res) {
+  console.log(req.files);
+  const upload_images = [];
+  try {
+    uploaded_images = await GalleryImage.bulkCreate(req.files);
+
+    res.status(200).json({
+      status: "success",
+      data: uploaded_images,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "failed",
+      data: error,
+    });
+  }
+}
+
+async function fetchSingleImage(req, res) {
+  const image_id = req.params.id;
   try {
     fetched_image = await GalleryImage.findByPk(image_id);
 
-    return { status: "success", data: fetched_image };
+    res.status(200).json({ status: "success", data: fetched_image });
   } catch (error) {
-    return { status: "failed", data: error };
+    res.status(200).json({ status: "failed", data: error });
   }
 }
 
-async function fetchImages(current_page = 1) {
+async function fetchImages(req, res) {
+  const current_page = req.params.page_number;
+
   const limit = 20;
   const offset = (current_page - 1) * 20;
   try {
     fetched_images = await GalleryImage.findAll({ limit, offset });
 
-    return { status: "success", data: fetched_images };
+    res.status(200).json({ status: "success", data: fetched_images });
   } catch (error) {
-    return { status: "failed", data: error };
+    res.status(500).json({ status: "failed", data: error });
   }
 }
 
